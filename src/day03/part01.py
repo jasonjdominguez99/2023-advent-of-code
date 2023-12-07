93ef14d7c48775cdbd8a6e286da5a6f87e35ca88
Jason Dominguez <jasonjdominguez99@gmail.com>
2023-12-07 00:14:23 +0000

    Complete day 3 part 1

--- src/day03/part01.py
+++ src/day03/part01.py
@@ -9,35 +9,81 @@ def get_schematic_from_file(fname):
 
     with open(fname) as f:
         for line in f:
-            # schematic.append(
-            #     [char for char in line.strip()]
-            # )
             schematic.append(line.strip())
 
-    print(schematic)
+    # print(schematic)
     return schematic
 
 
-def get_missing_part_numbers(schematic):
-    missing_part_nums = []
-
-    # TODO
-
-    return missing_part_nums
+def is_symbol(val):
+    return val != "." and re.match(r"\d", val) == None
 
 
-def display_sum_of_missing_part_nums(missing_part_nums):
-    print(sum(missing_part_nums))
+def is_missing(indices, row_num, schematic):
+    # print(indices)
+
+    if indices[0] > 0:
+        j = indices[0] - 1
+        val_to_check = schematic[row_num][j]
+        if is_symbol(val_to_check):
+            return False
+
+        indices = [indices[0] - 1] + indices
+
+    if indices[-1] < len(schematic[0]) - 1:
+        j = indices[-1] + 1
+        val_to_check = schematic[row_num][j]
+        if is_symbol(val_to_check):
+            return False
+
+        indices = indices + [indices[-1] + 1]
+
+    if row_num > 0:
+        i = row_num - 1
+        for j in indices:
+            val_to_check = schematic[i][j]
+            if is_symbol(val_to_check):
+                return False
+
+    if row_num < len(schematic) - 1:
+        i = row_num + 1
+        for j in indices:
+            val_to_check = schematic[i][j]
+            if is_symbol(val_to_check):
+                return False
+
+    return True
+
+
+def get_part_numbers(schematic):
+    part_nums = []
+
+    for i, row in enumerate(schematic):
+        # print(f"Row {i}")
+        nums = re.finditer(r"(\d+)", row)
+        for num in nums:
+            num_idxs = [*range(num.span()[0], num.span()[1])]
+
+            if not is_missing(num_idxs, i, schematic):
+                part_num = int(num.group(0))
+                part_nums.append(part_num)
+
+    # print(part_nums)
+    return part_nums
+
+
+def display_sum_of_part_nums(part_nums):
+    print(sum(part_nums))
 
 
 def main():
-    fname = "test_input.txt"
-    # fname = "input.txt"
+    # fname = "test_input.txt"
+    fname = "input.txt"
 
     engine = get_schematic_from_file(fname)
-    missing_part_nums = get_missing_part_numbers(engine)
+    part_nums = get_part_numbers(engine)
 
-    display_sum_of_missing_part_nums(missing_part_nums)
+    display_sum_of_part_nums(part_nums)
 
 
 if __name__ == "__main__":
